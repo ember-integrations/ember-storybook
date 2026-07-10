ebf2adb7296d072f74461c8b4341c0770f3a4d46
gossi <gossi@users.noreply.github.com>
2026-07-11 01:26:12 +0200

    minimal setup for compiling gts

--- ember-storybook/tsdown.config.js
+++ ember-storybook/tsdown.config.js
@@ -1,11 +1,33 @@
 import { defineConfig } from 'tsdown';
+import { Addon } from '@embroider/addon-dev/rollup';
+import { extensions } from '@embroider/vite';
+
+import { babel } from '@rollup/plugin-babel';
+
+const addon = new Addon({
+  srcDir: 'src',
+  destDir: 'dist'
+});
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/preset.ts', 'src/client/index.ts', 'src/client/config.ts', 'src/client/docs/config.ts', 'src/node/index.ts'],
   sourcemap: true,
   clean: true,
-  dts: true,
+  dts: false,
   deps: {
     neverBundle: [/^@ember/]
-  }
+  },
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      extensions
+    }),
+    addon.dependencies(), // misses on `@ember/reactive/collections`
+    addon.gjs(),
+    addon.declarations(
+      'declarations',
+      `ember-tsc --declaration --project ./tsconfig.json`
+    ),
+  ],
+  ignoreWatch: ['declarations/']
 });
