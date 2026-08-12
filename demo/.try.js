eb52c47654e36f0937be652d44a6df9dc7f0e38b
gossi <gossi@users.noreply.github.com>
2026-08-12 15:07:18 +0200

    test matrix

---
+++ demo/.try.js
@@ -0,0 +1,25 @@
+export default scenarios();
+
+function scenarios() {
+  return {
+    scenarios: [
+      latestEmberScenario('6.8'),
+      latestEmberScenario('6.12'),
+      latestEmberScenario('7.0'),
+      latestEmberScenario('latest'),
+      latestEmberScenario('beta'),
+      latestEmberScenario('alpha')
+    ]
+  };
+}
+
+function latestEmberScenario(tag) {
+  return {
+    name: `ember-${tag}`,
+    npm: {
+      devDependencies: {
+        'ember-source': `npm:ember-source@${tag}`
+      }
+    }
+  };
+}
