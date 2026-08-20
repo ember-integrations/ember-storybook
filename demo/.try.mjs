b51cb4170301e5d2dc4d579b838ddbfcac3d4dfa
gossi <gossi@users.noreply.github.com>
2026-08-20 19:11:33 +0200

    when does it fail?

--- demo/.try.mjs
+++ demo/.try.mjs
@@ -4,6 +4,9 @@ function scenarios() {
   return {
     scenarios: [
       latestEmberScenario('6.8'),
+      latestEmberScenario('6.9'),
+      latestEmberScenario('6.10'),
+      latestEmberScenario('6.11'),
       latestEmberScenario('6.12'),
       latestEmberScenario('7.0'),
       latestEmberScenario('latest'),
